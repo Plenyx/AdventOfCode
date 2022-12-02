61d0d800b2d40ef82ed26de8edde62f0a5c906c4
Plenyx <1507236+Plenyx@users.noreply.github.com>
2022-12-02 23:29:02 +0100

    Added day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,58 @@
+﻿using SharedLibs;
+
+// Part 1
+// A | X = Rock = 1
+// B | Y = Paper = 2
+// C | Z = Scissors = 3
+
+var input = Shared.LoadFromFileAsSpan("input.txt");
+
+var points = 0;
+
+foreach (ReadOnlySpan<char> line in input)
+{
+    if (line.Length < 3) continue;
+    var opponent = line[0] - 64;
+    var me = line[2] - 87;
+    var result = me - opponent;
+    if (Math.Abs(result) == 0)
+    {
+        points += 3 + me;
+        continue;
+    }
+    points += (((Math.Abs(result) == 2 && result < 0) || (Math.Abs(result) == 1 && result > 0)) ? 6 : 0) + me;
+}
+
+Console.WriteLine($"The solution for part 1 is: {points} points");
+
+// Part 2
+// A = Rock = 1
+// B = Paper = 2
+// C = Scissors = 3
+// X = Lose = 1
+// Y = Draw = 2
+// Z = Win = 3
+
+points = 0;
+
+foreach (ReadOnlySpan<char> line in input)
+{
+    if (line.Length < 3) continue;
+    var opponent = line[0] - 64;
+    var me = line[2] - 87;
+    if (me == 2)
+    {
+        points += 3 + opponent;
+        continue;
+    }
+    if (me == 1)
+    {
+        points += (opponent == 1 ? 3 : (opponent == 2) ? 1 : 2);
+        continue;
+    }
+    points += 6 + (opponent == 1 ? 2 : (opponent == 2) ? 3 : 1);
+}
+
+Console.WriteLine($"The solution for part 2 is: {points} points");
+
+Console.ReadLine();
