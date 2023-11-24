2b46d7a3244642d2d36804b758a0b8b3ad7d9a0a
Plenyx <1507236+Plenyx@users.noreply.github.com>
2023-11-25 00:40:45 +0100

    Day 1 solution

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,8 +1,46 @@
 ﻿using SharedLibs;
 
-var input = Shared.LoadFromFileAsSpan("input2.txt");
+var input = Shared.LoadFromFileAsSpan("input.txt");
 
 Shared.StartExecutionTimer();
+
+var previousNumber = int.MinValue;
+var increments = -1;
+
+foreach (ReadOnlySpan<char> line in input)
+{
+    var number = int.Parse(line);
+    if (number > previousNumber)
+    {
+        increments++;
+    }
+    previousNumber = number;
+}
+
+Console.WriteLine($"Solution for part 1: {increments}");
+
+previousNumber = int.MinValue;
+int window1 = int.MinValue, window2 = int.MinValue;
+increments = -1;
+var status = 0;
+
+foreach (ReadOnlySpan<char> line in input)
+{
+    status++;
+    var number = int.Parse(line);
+    if (status > 2)
+    {
+        if (window1 + window2 + number > previousNumber)
+        {
+            increments++;
+        }
+        previousNumber = window1 + window2 + number;
+    }
+    window1 = window2;
+    window2 = number;
+}
+
+Console.WriteLine($"Solution for part 1: {increments}");
 
 Shared.StopAndWriteExecutionTimer();
 
