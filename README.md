429538d1a07e4e6db4b04ab0e6dff28415cb5bbe
Plenyx <1507236+Plenyx@users.noreply.github.com>
2023-11-25 00:28:35 +0100

    2021

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code 2021
+My C# solutions to Advent of Code 2021.
---
+++ SharedLibs/Shared.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace SharedLibs
+{
+    public static class Shared
+    {
+        private static readonly Stopwatch executionTimer = new();
+
+        public static ReadOnlySpan<string> LoadFromFileAsSpan(string fileName)
+        {
+            try
+            {
+                return File.ReadAllLines(fileName).AsSpan();
+            }
+            catch
+            {
+                return Array.Empty<string>();
+            }
+        }
+
+        public static void StartExecutionTimer()
+        {
+            executionTimer.Start();
+        }
+
+        public static TimeSpan StopAndReturnExecutionTimer()
+        {
+            executionTimer.Stop();
+            var timer = executionTimer.Elapsed;
+            executionTimer.Restart();
+            return timer;
+        }
+
+        public static void StopAndWriteExecutionTimer()
+        {
+            Console.WriteLine($"Program finished after: {StopAndReturnExecutionTimer()}");
+        }
+    }
+}
