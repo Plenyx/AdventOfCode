1ce6253f67301d6b376e4f5e17ecc89ced9613be
Plenyx <1507236+Plenyx@users.noreply.github.com>
2023-12-14 19:47:19 +0100

    Day 01 + Day 02

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -2,7 +2,82 @@
 
 var input = Shared.LoadFromFileAsSpan("input.txt");
 
+ReadOnlySpan<char> searchChars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+var letteredWords = new Dictionary<char[], int>() {
+    { new char[] { 'o', 'n', 'e' }, 1 },
+    { new char[] { 't', 'w', 'o' }, 2 },
+    { new char[] { 't', 'h', 'r', 'e', 'e' }, 3 },
+    { new char[] { 'f', 'o', 'u', 'r' }, 4 },
+    { new char[] { 'f', 'i', 'v', 'e' }, 5 },
+    { new char[] { 's', 'i', 'x' }, 6 },
+    { new char[] { 's', 'e', 'v', 'e', 'n' }, 7 },
+    { new char[] { 'e', 'i', 'g', 'h', 't' }, 8 },
+    { new char[] { 'n', 'i', 'n', 'e' }, 9 },
+};
+
+var part1Result = 0;
+var part2Result = 0;
+
+var firstNumber = 0;
+var secondNumber = 0;
+
 Shared.StartExecutionTimer();
+
+foreach (ReadOnlySpan<char> line in input)
+{
+    var firstNumberIndex = line.IndexOfAny(searchChars);
+    if (firstNumberIndex < 0)
+    {
+        continue;
+    }
+    firstNumber = line[firstNumberIndex] - 48;
+    var secondNumberIndex = line.LastIndexOfAny(searchChars);
+    secondNumber = line[secondNumberIndex] - 48;
+    part1Result += firstNumber * 10 + secondNumber;
+}
+
+Console.WriteLine($"Solution to part 1: {part1Result}");
+
+foreach (ReadOnlySpan<char> line in input)
+{
+    firstNumber = 0;
+    secondNumber = 0;
+    var firstNumberIndex = line.IndexOfAny(searchChars);
+    var firstWordIndex = int.MaxValue;
+    var lastWordIndex = -1;
+    foreach (var word in letteredWords.Keys)
+    {
+        ReadOnlySpan<char> spannedWord = word;
+        var localFirstWordIndex = line.IndexOf(spannedWord);
+        if (localFirstWordIndex < firstWordIndex && localFirstWordIndex > -1)
+        {
+            firstWordIndex = localFirstWordIndex;
+            firstNumber = letteredWords[word];
+        }
+        var localLastWordIndex = line.LastIndexOf(spannedWord);
+        if (localLastWordIndex > lastWordIndex)
+        {
+            lastWordIndex = localLastWordIndex;
+            secondNumber = letteredWords[word];
+        }
+    }
+    if (firstNumberIndex < 0 && firstWordIndex < 0)
+    {
+        continue;
+    }
+    if (firstNumberIndex > -1 && (firstNumberIndex < firstWordIndex) || (firstWordIndex == -1))
+    {
+        firstNumber = line[firstNumberIndex] - 48;
+    }
+    var secondNumberIndex = line.LastIndexOfAny(searchChars);
+    if (secondNumberIndex > lastWordIndex)
+    {
+        secondNumber = line[secondNumberIndex] - 48;
+    }
+    part2Result += firstNumber * 10 + secondNumber;
+}
+
+Console.WriteLine($"Solution to part 2: {part2Result}");
 
 Shared.StopAndWriteExecutionTimer();
 
---
+++ Day02/Program.cs
@@ -0,0 +1,97 @@
+﻿using SharedLibs;
+using System.Text.RegularExpressions;
+
+var input = Shared.LoadFromFileAsSpan("input.txt");
+
+var redRegEx = new Regex(@"(\d+) red", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+var blueRegEx = new Regex(@"(\d+) blue", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+var greenRegEx = new Regex(@"(\d+) green", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+Shared.StartExecutionTimer();
+
+var part1Result = 0;
+
+foreach (var line in input)
+{
+    var gameSplit = line.Split(':');
+    var gameId = int.Parse(gameSplit[0][5..]);
+    var sets = gameSplit[1].Split(';');
+
+    var failed = false;
+
+    foreach (var set in sets)
+    {
+        var red = 12;
+        var green = 13;
+        var blue = 14;
+        foreach (Match match in redRegEx.Matches(set))
+        {
+            red -= int.Parse(match.Groups[1].ValueSpan);
+        }
+
+        foreach (Match match in greenRegEx.Matches(set))
+        {
+            green -= int.Parse(match.Groups[1].ValueSpan);
+        }
+
+        foreach (Match match in blueRegEx.Matches(set))
+        {
+            blue -= int.Parse(match.Groups[1].ValueSpan);
+        }
+        if (red < 0 || green < 0 || blue < 0)
+        {
+            failed = true;
+            break;
+        }
+    }
+    if (!failed)
+    {
+        part1Result += gameId;
+    }
+}
+
+Console.WriteLine($"Solution to part 1: {part1Result}");
+
+var part2Result = 0;
+
+foreach (var line in input)
+{
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+
+    foreach (Match match in redRegEx.Matches(line))
+    {
+        var redCount = int.Parse(match.Groups[1].ValueSpan);
+        if (redCount > red)
+        {
+            red = redCount;
+        }
+    }
+
+    foreach (Match match in greenRegEx.Matches(line))
+    {
+        var greenCount = int.Parse(match.Groups[1].ValueSpan);
+        if (greenCount > green)
+        {
+            green = greenCount;
+        }
+    }
+
+    foreach (Match match in blueRegEx.Matches(line))
+    {
+        var blueCount = int.Parse(match.Groups[1].ValueSpan);
+        if (blueCount > blue)
+        {
+            blue = blueCount;
+        }
+    }
+
+    part2Result += red * green * blue;
+}
+
+Console.WriteLine($"Solution to part 2: {part2Result}");
+
+Shared.StopAndWriteExecutionTimer();
+
+Console.ReadLine();
