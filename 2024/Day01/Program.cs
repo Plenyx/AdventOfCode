9b3ecc21627a8a87a095ef63825e6852259dda59
Plenyx <1507236+Plenyx@users.noreply.github.com>
2024-12-06 01:06:07 +0100

    20224 - Day01 & Day02

---
+++ 2024/Day01/Program.cs
@@ -0,0 +1,42 @@
+﻿using SharedLibs;
+
+var input = Shared.LoadFromFileAsSpan("input.txt");
+
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+var result = 0;
+var result2 = 0;
+
+foreach (ReadOnlySpan<char> item in input)
+{
+    var firstSpace = item.IndexOf(" ", StringComparison.Ordinal);
+    var lastSpace = item.LastIndexOf(" ", StringComparison.Ordinal);
+    var firstNumber = item[..firstSpace];
+    list1.Add(int.Parse(firstNumber));
+    var secondNumber = item[(lastSpace + 1)..];
+    list2.Add(int.Parse(secondNumber));
+}
+
+list1 = [.. list1.OrderBy(x => x)];
+list2 = [.. list2.OrderBy(x => x)];
+
+Shared.StartExecutionTimer();
+
+for (int i = 0; i < list1.Count; i++)
+{
+    result += Math.Abs(list1[i] - list2[i]);
+}
+
+Console.WriteLine($"Solution for part 1: {result}");
+
+for (int i = 0; i < list1.Count; i++)
+{
+    var number = list1[i];
+    var times = list2.Where(x => x == number).Count();
+    result2 += number * times;
+}
+
+Console.WriteLine($"Solution for part 2: {result2}");
+
+Shared.StopAndWriteExecutionTimer();
---
+++ 2024/Day02/Program.cs
@@ -0,0 +1,76 @@
+﻿using SharedLibs;
+
+var input = Shared.LoadFromFileAsSpan("input.txt");
+
+var result = 0;
+var result2 = 0;
+
+Shared.StartExecutionTimer();
+
+int areNumbersSafe(List<string> items)
+{
+    var previousPreviousNumber = int.Parse(items[0]);
+    var previousNumber = int.Parse(items[1]);
+    var startDiff = Math.Abs(previousPreviousNumber - previousNumber);
+    if (startDiff < 1 || startDiff > 3)
+    {
+        return 0;
+    }
+    for (int i = 2; i < items.Count; i++)
+    {
+        var number = int.Parse(items[i]);
+        var diff = Math.Abs(number - previousNumber);
+        if (diff < 1 || diff > 3)
+        {
+            return 0;
+        }
+        if (previousPreviousNumber > previousNumber)
+        {
+            if (previousNumber < number)
+            {
+                return 0;
+            }
+        }
+        else if (previousPreviousNumber < previousNumber)
+        {
+            if (previousNumber > number)
+            {
+                return 0;
+            }
+        }
+        previousPreviousNumber = previousNumber;
+        previousNumber = number;
+    }
+    return 1;
+}
+
+int areNumbersSafeDamp(List<string> items)
+{
+    if (areNumbersSafe([..items]) == 1)
+    {
+        return 1;
+    }
+    for (int i = 0; i < items.Count; i++)
+    {
+        List<string> newItems = new(items);
+        newItems.RemoveAt(i);
+        if (areNumbersSafe([..newItems]) == 1)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+foreach (var item in input)
+{
+    var items = item.Split(" ").ToList();
+    result += areNumbersSafe(items);
+    result2 += areNumbersSafeDamp(items);
+}
+
+
+Console.WriteLine($"Solution for part 1: {result}");
+Console.WriteLine($"Solution for part 2: {result2}");
+
+Shared.StopAndWriteExecutionTimer();
